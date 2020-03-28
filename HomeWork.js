f9d19111d2effe0a0a474e2cf954e253fc5384ee
SQ-DM <d4417n@yandex.ru>
2020-03-28 17:35:10 +0300

    Варианты циклов

--- HomeWork.js
+++ HomeWork.js
@@ -1,5 +1,5 @@
 "use strict";
-let money = prompt("Ваш бюджет на месяц?", "");
+let money = +prompt("Ваш бюджет на месяц?", "");
 let time = prompt("Введите дату в формате YYYY-MM-DD", "");
 let appData = {
   budget: money,
@@ -29,5 +29,64 @@ for (let i = 0; i < 2; i++) {
     i--;
   }
 }
+appData.moneyPerDay = appData.budget / 30;
 
-alert(appData.budget / 30);
+//-------Цикл Wile
+// let i = 0;
+// while (i < 2) {
+//   let a = prompt("Введите обязательную статью расходов в этом месяце", "");
+//   let b = prompt("Во сколько обойдется?", "");
+
+//   if (
+//     typeof a === "string" &&
+//     typeof a != null &&
+//     typeof b != null &&
+//     a != "" &&
+//     b != "" &&
+//     a.length < 50
+//   ) {
+//     console.log("Done!");
+//     appData.expenses[a] = b;
+//   } else {
+//     console.log("Not Done!");
+//     i--;
+//   }
+//   i++;
+// }
+// appData.moneyPerDay = appData.budget / 30;
+
+// Цикл Do Wile
+// let i = 0;
+// do {
+//   let a = prompt("Введите обязательную статью расходов в этом месяце", "");
+//   let b = prompt("Во сколько обойдется?", "");
+
+//   if (
+//     typeof a === "string" &&
+//     typeof a != null &&
+//     typeof b != null &&
+//     a != "" &&
+//     b != "" &&
+//     a.length < 50
+//   ) {
+//     console.log("Done!");
+//     appData.expenses[a] = b;
+//   } else {
+//     console.log("Not Done!");
+//     i--;
+//   }
+// } while (i < 2);
+
+appData.moneyPerDay = appData.budget / 30;
+
+alert("Ежедневный бюджет: " + appData.moneyPerDay);
+
+if (appData.moneyPerDay < 100) {
+  console.log("Минимальный уровень дохода");
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+  console.log("Средний уровень дохода");
+} else if (appData.moneyPerDay > 2000) {
+  console.log("Высокий уровень дохода");
+} else {
+  console.log("Произошла ошибка");
+}
