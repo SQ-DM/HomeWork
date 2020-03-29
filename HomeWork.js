4d0b694e51b221fee583fcfa2865b42570078485
SQ-DM <d4417n@yandex.ru>
2020-03-29 21:35:37 +0300

    Домашнее задание №3

--- HomeWork.js
+++ HomeWork.js
@@ -1,37 +1,103 @@
 "use strict";
-let money = +prompt("Ваш бюджет на месяц?", "");
-let time = prompt("Введите дату в формате YYYY-MM-DD", "");
+let money, time;
+
+function start() {
+  money = +prompt("Ваш бюджет на месяц?", "");
+  time = prompt("Введите дату в формате YYYY-MM-DD", "");
+
+  while (isNaN(money) || money == "" || money == null) {
+    money = +prompt("Ваш бюджет на месяц?", "");
+  }
+}
+start();
+
 let appData = {
   budget: money,
   timeData: time,
   expenses: {},
   optionalExpenses: {},
   income: [],
-  savings: false
+  savings: true
 };
 
-for (let i = 0; i < 2; i++) {
-  let a = prompt("Введите обязательную статью расходов в этом месяце", "");
-  let b = prompt("Во сколько обойдется?", "");
+function chooseExpenses() {
+  for (let i = 0; i < 2; i++) {
+    let a = prompt("Введите обязательную статью расходов в этом месяце", "");
+    let b = prompt("Во сколько обойдется?", "");
 
-  if (
-    typeof a === "string" &&
-    typeof a != null &&
-    typeof b != null &&
-    a != "" &&
-    b != "" &&
-    a.length < 50
-  ) {
-    console.log("Done!");
-    appData.expenses[a] = b;
-  } else {
-    console.log("Not Done!");
-    i--;
+    if (
+      typeof a === "string" &&
+      typeof a != null &&
+      typeof b != null &&
+      a != "" &&
+      b != "" &&
+      a.length < 50
+    ) {
+      console.log("Done!");
+      appData.expenses[a] = b;
+    } else {
+      console.log("Not Done!");
+      i = i - 1;
+    }
   }
 }
-appData.moneyPerDay = appData.budget / 30;
+chooseExpenses();
 
-//-------Цикл Wile
+function chooseOptExpenses() {
+  for (let i = 0; i < 3; i++) {
+    let c = prompt("Введите необязательную статью расходов", "");
+    let d = prompt("Во сколько обойдеться?", "");
+    if (
+      typeof c === "string" &&
+      typeof c != null &&
+      typeof c != "" &&
+      typeof d != null &&
+      typeof d != "" &&
+      c.length < 50
+    ) {
+      console.log("Done too!");
+      appData.optionalExpenses[c] = d;
+    } else {
+      console.log("Not Done!");
+      i = i--;
+    }
+  }
+}
+chooseOptExpenses();
+
+// ----Функция расчета дневного бюджета
+function detectDayBudget() {
+  appData.moneyPerDay = (appData.budget / 30).toFixed();
+  alert("Ежедневный бюджет: " + appData.moneyPerDay);
+}
+detectDayBudget();
+
+//----Функция расчета достатка
+function detectLevel() {
+  if (appData.moneyPerDay < 100) {
+    console.log("Минимальный уровень дохода");
+  } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+    console.log("Средний уровень дохода");
+  } else if (appData.moneyPerDay > 2000) {
+    console.log("Высокий уровень дохода");
+  } else {
+    console.log("Произошла ошибка");
+  }
+}
+detectLevel();
+
+function checkSavings() {
+  if (appData.savings == true) {
+    let save = +prompt("Какова сумма процентов");
+    let percent = +prompt("Каков процент?");
+
+    appData.monthIncome = (save / 100 / 12) * percent;
+    alert("Доход в месяц с вашего депозита" + appData.monthIncome);
+  }
+}
+checkSavings();
+
+//-------Цикл While
 // let i = 0;
 // while (i < 2) {
 //   let a = prompt("Введите обязательную статью расходов в этом месяце", "");
@@ -55,7 +121,7 @@ appData.moneyPerDay = appData.budget / 30;
 // }
 // appData.moneyPerDay = appData.budget / 30;
 
-// Цикл Do Wile
+// ------Цикл Do While
 // let i = 0;
 // do {
 //   let a = prompt("Введите обязательную статью расходов в этом месяце", "");
@@ -76,17 +142,3 @@ appData.moneyPerDay = appData.budget / 30;
 //     i--;
 //   }
 // } while (i < 2);
-
-appData.moneyPerDay = appData.budget / 30;
-
-alert("Ежедневный бюджет: " + appData.moneyPerDay);
-
-if (appData.moneyPerDay < 100) {
-  console.log("Минимальный уровень дохода");
-} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
-  console.log("Средний уровень дохода");
-} else if (appData.moneyPerDay > 2000) {
-  console.log("Высокий уровень дохода");
-} else {
-  console.log("Произошла ошибка");
-}
